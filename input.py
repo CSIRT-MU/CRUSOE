d5fc50977b63ac5b141a0c4ad3990aa6110022b7
Vladimír Bouček <xboucek@fi.muni.cz>
2021-10-19 00:10:28 +0200

    JSON loading added

--- input.py
+++ input.py
@@ -2,12 +2,14 @@ from sys import argv
 from getopt import getopt, GetoptError
 from ipaddress import ip_address
 from re import match
+from json import load
 
 
 class Input:
     def __init__(self):
         self.ip = None
         self.domain = None
+        self.config_path = None
 
     def parse_options(self):
         """
@@ -15,31 +17,48 @@ class Input:
         appropriately.
         :return: True if options were obtained correctly, False otherwise
         """
+
+        result = False
+
         try:
             # -i | --ip     -> IP input
             # -d | --domain -> domain input
-            opts, _ = getopt(argv[1:], "i:d:", ["ip=", "domain="])
+            # -p | --path   –> config path
+            opts, _ = getopt(argv[1:], "i:d:c:", ["ip=", "domain=", "config="])
 
         except GetoptError:
             # Unsupported options
             print("Error - Invalid options")
-            return False
+            return result
 
         if not opts:
             # No options were obtained
             print("Error - No arguments were given")
-            return False
+            return result
 
         for opt, arg in opts:
             if opt in ("-i", "--ip"):
-                self.ip = self.check_ip_address(arg)
-                return True
+                self.ip = self._check_ip_address(arg)
+                result = True
             elif opt in ("-d", "--domain"):
-                self.domain = self.check_domain(arg)
-                return True
+                self.domain = self._check_domain(arg)
+                result = True
+            elif opt in ("-c", "--config"):
+                self.config_path = arg
+
+        return result
+
+    def load_config(self):
+        if self.config_path is None:
+            path = "default_config.json"
+        else:
+            path = self.config_path
+
+        with open(path, 'r') as config_stream:
+            return load(config_stream)
 
     @staticmethod
-    def check_ip_address(address):
+    def _check_ip_address(address):
         """
         Checks if given string is a valid IP address
         :param address: IP address in string
@@ -53,7 +72,7 @@ class Input:
         return ip
 
     @staticmethod
-    def check_domain(domain):
+    def _check_domain(domain):
         """
         Checks if given string is a domain name
         :param domain: Domain name
