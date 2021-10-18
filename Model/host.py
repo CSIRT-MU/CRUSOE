05d6d3efb8818627f47216767b78dac92ae0bc3d
Vladimír Bouček <xboucek@fi.muni.cz>
2021-10-19 00:20:20 +0200

    CVE and event counts added.

--- Model/host.py
+++ Model/host.py
@@ -1,3 +1,6 @@
+from Model.software_component import SoftwareComponent
+
+
 class Host:
     """
     Represents one host in the network.
@@ -5,14 +8,14 @@ class Host:
     def __init__(self, ip, domains, os_cpe):
         self.ip = ip
         self.domains = domains
-        self.os_cpe = os_cpe
+        self.os_component = SoftwareComponent("os_component", os_cpe)
         self.software_components = []
         self.network_services = []
 
     def __str__(self):
         return f"IP: {self.ip}\n" \
                f"DOMAIN(s): {self.domains}\n" \
-               f"OS: {self.os_cpe}\n" \
+               f"OS: {self.os_component}\n" \
                f"SW: {[sw.__str__() for sw in self.software_components]}\n" \
                f"NS: {[ns.__str__() for ns in self.network_services]}"
 
@@ -21,8 +24,9 @@ class HostWithScore(Host):
     """
     Represents nearby hosts evaluated with score and distance
     """
-    def __init__(self, ip, domains, os_cpe):
-        Host.__init__(self, ip, domains, os_cpe)
+    def __init__(self, ip, domains, os_cpe, cve_count, event_count):
+        super().__init__(ip, domains, os_cpe)
+        self.cve_count = cve_count
+        self.event_count = event_count
         self.risk_score = None
         self.distance = None
-
