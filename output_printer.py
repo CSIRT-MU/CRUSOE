e9c341084cdfa9df4ece65f29526748f5d8c2139
Vladimír Bouček <xboucek@fi.muni.cz>
2021-11-23 23:05:59 +0100

    Printing of host table improved.

--- output_printer.py
+++ output_printer.py
@@ -1,6 +1,5 @@
 from math import inf
 
-
 class OutputPrinter:
     # ASCII text edit symbols
     PINK = '\033[95m'
@@ -13,44 +12,75 @@ class OutputPrinter:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-    HEADERS = [
-        ("IP ADDRESS", 20),
-        ("DOMAIN(S)", 50),
-        ("RISK", 10)
-    ]
+    HEADERS = ["IP ADDRESS", "DOMAIN(S)", "RISK"]
+
+    WIDTHS = [20, 50, 10]
 
     def __init__(self, limit, verbose):
-        self.column_width = 40
-        # Set limit to infinity if no limit was given from options
-        self.limit = inf if limit is None else limit
+        self.limit = limit
         self.verbose = verbose
 
+    def print_attacked_host(self, host):
+        print(self.BLUE + self.BOLD + "ATTACKED HOST:" + self.END)
+        print(host)
+
     def print_number_of_hosts(self, number_of_hosts, max_distance):
-        print()
         print(self.BLUE +
               f"Found {number_of_hosts} hosts to maximum distance of "
               f"{max_distance}:")
-        if self.limit < inf:
+        # Show how much hosts is actually being printed (defined by limit
+        # given as a option)
+        if self.limit is not None and self.limit < number_of_hosts:
             print(self.BLUE +
                   f"Displaying {self.limit} hosts.")
         print()
 
     def print_host_list(self, host_list):
-        # Print headers
-        print(self.GREEN + " ", end="")
-        for header, width in self.HEADERS:
-            print(self.BOLD + self.BLUE + str.center(header, width) + " ",
-                  end="")
+        """
+        Prints given host list to stdout formatted in a table.
+        :param host_list: List of hosts to print
+        :return: None
+        """
+        table_color = self.YELLOW
+
+        # Print table header
+        self.__print_host_list_header(self.BLUE)
+
+        # Print table
+        self.__print_horizontal_separator(table_color)
+
+        # List slices doesn't work with none -> use inf instead of None
+        for host in host_list[:inf if self.limit is None else self.limit]:
+            self.__print_host_in_table(host, table_color)
+            self.__print_horizontal_separator(self.YELLOW)
+
+    def __print_horizontal_separator(self, color):
+        print(color, end="")
+        for width in self.WIDTHS:
+            print(f"+{width * '-'}", end="")
+        print("+" + self.END)
+
+    def __print_host_list_header(self, color):
+        print(color + self.BOLD, end="")
+        for header, width in zip(self.HEADERS, self.WIDTHS):
+            print(" " + str.center(header, width), end="")
         print(self.END)
 
-        # Print host list
-        for host in host_list[:self.limit]:  # Limit number of
-            print(self.YELLOW + "|", end="")
+    def __print_host_in_table(self, host, color):
+        print(color + "|", end="")
 
-            for item, width in [(str(host.ip), 20), (host.domains[0], 50), (str(round(host.risk, 4)), 10)]:
-                print(self.YELLOW + str.center(item, width) + "|", end="")
+        for item, width in [(str(host.ip), 20), (host.domains[0], 50),
+                            (str(round(host.risk, 4)), 10)]:
+            print(self.YELLOW + str.center(item, width) + "|", end="")
+        if len(host.domains) > 1:
+            print()
+            for domain in host.domains[1:]:
+                print("|" + 20 * " " + "|", end="")
+                print(str.center(domain, 50), end="")
+                print("|" + 10 * " " + "|")
+        else:
             print()
 
-    def print_attacked_host(self, host):
-        print(self.BLUE + self.BOLD + "ATTACKED HOST:" + self.END)
-        print(host)
+        print(self.END, end="")
+
+
